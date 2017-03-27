3a4dde4a4a22d7b768e79e5ca26969a54890bc46
Mikołaj Stępczak <stepczak.mikolaj@gmail.com>
2017-03-27 21:45:01 +0200

    dodanie panelu zarządzania fiszkami

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,22 @@ var app = angular.module('cartsApp', []);
 app.controller('cartsCtrl', function($scope) {
     $scope.currentCart = 0;
     $scope.loader = true;
+    $scope.question = "";
+    $scope.answer = "";
     $scope.carts = [
             {
-                id: 0,
                 question: "Gdzie urodził się Fryderyk Chopin?",
                 answer: "Żelazowa Wola"
             },
             {
-                id: 1,
                 question: "W który roku zmarł Ludwig van Beethoven?",
                 answer: "1827r."
             },
             {
-                id: 2,
                 question: "W którym roku urodził się Wolfgang Amadeus Mozart?",
                 answer: "1756r."
             },
             {
-                id: 3,
                 question: "Gdzie umarł Johannes Brahms?",
                 answer: "W Wiedniu"
             }
@@ -34,6 +32,41 @@ app.controller('cartsCtrl', function($scope) {
             $scope.carts[index].hover = true;
         }
     }
+    
+    $scope.toggleConfig = function(index) {
+        if($scope.config) {
+            $scope.config = false;
+        } else {
+            $scope.config = true;
+        }
+    }
+    
+    $scope.removeCart = function(index) {
+        if($scope.carts[index].active) {
+            $scope.changeCart(index+1);
+            $scope.changeCart(index-1);
+        }
+        if(index <= $scope.currentCart) $scope.currentCart--;
+        $scope.carts.splice(index, 1); 
+        
+    };
+    
+    $scope.addCart = function(index) {
+        if($scope.question != "" && $scope.answer != "") {
+            $scope.carts.push({
+                question: $scope.question,
+                answer: $scope.answer
+            });
+            if($scope.carts.length==1){
+                $scope.carts[0].active=true;
+                $scope.currentCart = 0
+            }
+            $scope.question = "";
+            $scope.answer = "";
+        } else {
+            alert("Wprowadź pytanie i odpowiedź.");
+        }
+    };
     
     $scope.changeCart = function(index) {
         if(index >= 0 && index < $scope.carts.length) {
